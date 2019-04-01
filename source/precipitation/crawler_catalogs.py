79cb0473ef9a25473f2a56cd9b570f752c51839a
Andy Barrett <apbarret@nsidc.org>
2019-04-01 11:29:54 -0600

    Dictionary for NASA crawler top-level xml pages

---
+++ source/precipitation/crawler_catalogs.py
@@ -0,0 +1,8 @@
+# Contains XML catalogs for MERRA2 monthly and diurnal data
+
+diurnal = {
+    'V10M': 'https://goldsmr4.gesdisc.eosdis.nasa.gov/opendap/MERRA2/M2T1NXSLV.5.12.4/catalog.xml',
+    'U10M': 'https://goldsmr4.gesdisc.eosdis.nasa.gov/opendap/MERRA2/M2T1NXSLV.5.12.4/catalog.xml',
+    'SLP': 'https://goldsmr4.gesdisc.eosdis.nasa.gov/opendap/MERRA2/M2T1NXSLV.5.12.4/catalog.xml',
+}
+
