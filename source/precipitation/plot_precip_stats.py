bd989fd8b4b95b7fb29ad830bcb02e45cb1ef5b7
Andy Barrett <apbarret@nsidc.org>
2019-05-02 17:19:48 -0600

    extended to plot all precip stats - no longer need separate scripts

--- source/precipitation/plot_precip_stats.py
+++ source/precipitation/plot_precip_stats.py
@@ -3,81 +3,93 @@
 #
 #----------------------------------------------------------------------
 
-import matplotlib
+#import matplotlib
+#matplotlib.use('Agg')
 
-matplotlib.use('Agg')
-
-import xarray as xr
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
+
 import numpy as np
 
-fili = {'CFSR': '/disks/arctic5_raid/abarrett/CFSR/PRATE/CFSR.flxf06.gdas.PRECIP_STATS.accumulation.annual.Nh50km.nc',
-        'ERAI': '/disks/arctic5_raid/abarrett/ERA_Interim/daily/PRECTOT/era_interim.PRECIP_STATS.accumulation.annual.Nh50km.nc',
-        'JRA55': '/projects/arctic_scientist_data/Reanalysis/JRA55/daily/TOTPREC/JRA55.fcst_phy2m.PRECIP_STATS.accumulation.annual.Nh50km.nc',
-        'MERRA': '/disks/arctic5_raid/abarrett/MERRA/daily/PRECTOT/MERRA.prod.PRECIP_STATS.assim.tavg1_2d_flx_Nx.accumulation.annual.Nh50km.nc4',
-        'MERRA2': '/disks/arctic5_raid/abarrett/MERRA2/daily/PRECTOT/MERRA2.tavg1_2d_flx_Nx.PRECIP_STATS.accumulation.annual.Nh50km.nc4',}
+import plotutils
 
-maskFile = '/home/apbarret/src/utilities/data/arctic_mask.ocean.Nh50km.nc'
 
-def get_data(reanalysis, field):
+# Default range of values
+plot_params = { 
+    'precTot': {'intervals': (0., 1000., 11),
+                'extend': 'max', 'colormap': 'viridis_r', 'cb_label': 'mm'},
+    'wetdayTot': {'intervals': (0., 1000., 11),
+                  'extend': 'max', 'colormap': 'viridis_r', 'cb_label': 'mm'},
+    'nwetdays': {'intervals': (0., 250., 11),
+                 'extend': 'max', 'colormap': 'plasma_r', 'cb_label': 'N'},
+    'fwetdays': {'intervals': (0., 1., 11),
+                 'extend': 'neither', 'colormap': 'plasma_r', 'cb_label': 'fraction'},
+    'wetdayAve': {'intervals': (1., 5., 9),
+                  'extend': 'max', 'colormap': 'viridis_r', 'cb_label': 'mm'},
+}
+
+
+def get_plot_params(field):
+    """Returns colormap and norm for a given field"""
+    cmap = plotutils.get_cmap(plot_params[field]['colormap'])
+    bounds = np.linspace(*plot_params[field]['intervals'])
+    norm = mcolors.BoundaryNorm(boundaries=bounds, ncolors=cmap.N)
+    
+    return cmap, norm, plot_params[field]['extend'], plot_params[field]['cb_label']
+
+
+def plot_precip_stats(field, height=5., width=8.5, fix_aspect=True, 
+                      outdiri='.', nosave=False):
+    """Generates plot of PRECIP_STATS field for the six reanalyses in
+    the Snow on Sea Ice precipitation paper
+
+    field - Data field to plot: precTot, wetdayTot, nwetdays, fwetdays, wetdayAve
+
+    height - height of figure (default 5")
+             (is adjusted to 5/8.5 of width if fix_aspect True)
+    width - width of figure (default 8.5")
+    fix_aspect - fixes aspect to 5/8.5 if True (default True)
+    outdiri - Sets output directory (default .)
+    nosave - Plot is not written to file but displayed using plt.show()
     """
-    Get specified PRECIP_STAT field for a given reanalysis
-    """
-    ds = xr.open_dataset(fili[reanalysis])
-    da = ds[field]
-    if reanalysis == 'JRA55': da = da*0.1
-    year = ds['time'].dt.year
-    return da.isel( time=( (year >= 1981) & (year <= 2015) ) ).mean(dim='time')
+
+    cmap, norm, cb_extend, cb_units = get_plot_params(field)
+
+    ds = plotutils.load_data(field, 'mean')
+    fig, ax = plotutils.make_figure(ds, norm=norm, cmap=cmap, height=height, width=width,
+                                    cb_extend=cb_extend, cb_units=cb_units)
+
+    if nosave:
+        plt.show()
+    else:
+        fileout = os.path.join(outdiri,
+                           f'arctic_precipitation.accumulation_period.climatology.{field}.cfsr_totprec.png')
+        print ('Saving plot to ' + fileout)
+        fig.savefig(fileout)
     
-def plot_precip_stats():
-
-    from plotutils import EASE_North
-
-    field = 'precTot'
-    
-    map_proj = EASE_North()
-    extent = [-9036842.762500, 9036842.762500, -9036842.762500, 9036842.762500]
-    lim = 3000000
-    
-    bounds = np.arange(50,1000.,50)
-    norm = mcolors.BoundaryNorm(boundaries=bounds, ncolors=256)
-
-    fig = plt.figure(figsize=(15,7))
-    
-    # Plot field
-    ax = []
-    for ip, renm in enumerate(list(fili.keys())):
-
-        print ('Plotting {:s}'.format(renm))
-
-        var = get_data(renm, field)
-        print (var.min(), var.max())
-        
-        ax.append( plt.subplot(2, 3, ip+1, projection=map_proj, xlim=lim, ylim=lim) )
-        ax[ip].set_extent([-180.,180.,65.,90.], ccrs.PlateCarree())
-        img = ax[ip].imshow(var, norm=norm, cmap='YlGnBu', 
-                            interpolation='none', origin='upper',
-                            extent=extent, transform=map_proj)
-        ax[ip].coastlines()
-        ax[ip].gridlines()
-        ax[ip].set_title(renm)
-
-    fig.colorbar(img, ax=ax[-1], extend='both', orientation='vertical')
-    
-    fig.savefig('arctic_precipitation.accumulation_period.climatology.{:s}.png'.format(field))
-
     return
+
 
 if __name__ == "__main__":
 
     import argparse
 
-    #parser = argparse.ArgumentParser(description="Calculates time series of AO average precip stats")
-    #parser.add_argument('reanalysis', type=str, help='Reanalysis to process')
-    #args = parser.parse_args()
+    parser = argparse.ArgumentParser(description="Plots climatologies of PRECIP_STATS for precipitation paper")
+    parser.add_argument('field', type=str,
+                        help='Data field to plot: precTot, wetdayTot, nwetdays, fwetdays, wetdayAve')
+    parser.add_argument('--height', type=float, default=5,
+                        help='Height of figure in inches (default 5")')
+    parser.add_argument('--width', type=float, default=8.5,
+                        help='Width of figure in inches (default 8.5")')
+    parser.add_argument('--fix_aspect', action='store_true',
+                        help='Adjust height to meet aspect of 5/8.5')
+    parser.add_argument('--outdiri', type=str, default='.',
+                        help='Output dirpath to save figure to')
+    parser.add_argument('--nosave', action='store_true',
+                        help="Don't save figure, display on screen")
+    
+    args = parser.parse_args()
 
-    plot_precip_stats() #(args.reanalysis)
+    plot_precip_stats(args.field, height=args.height, width=args.width, fix_aspect=args.fix_aspect,
+                      outdiri=args.outdiri, nosave=args.nosave)
     
