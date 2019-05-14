421b99631932dbd4f88ac72d5b04782021b8d8fe
Andy Barrett <apbarret@nsidc.org>
2019-05-14 15:46:32 -0600

    changed filepaths, calculate drizzle from precTot and wetdayTot

--- source/precipitation/precip_stats_to_time_series.py
+++ source/precipitation/precip_stats_to_time_series.py
@@ -15,13 +15,14 @@ import cartopy.feature as cfeature
 import numpy as np
 
 from constants import maskFile
+from constants import accumulation_period_filepath as fili
 
-fili = {'CFSR': '/disks/arctic5_raid/abarrett/CFSR/TOTPREC/CFSR.pgbh01.gdas.PRECIP_STATS.accumulation.annual.Nh50km.nc4',
-        'ERAI': '/disks/arctic5_raid/abarrett/ERA_Interim/daily/PRECTOT/era_interim.PRECIP_STATS.accumulation.annual.Nh50km.nc',
-        'JRA55': '/projects/arctic_scientist_data/Reanalysis/JRA55/daily/TOTPREC/JRA55.fcst_phy2m.PRECIP_STATS.accumulation.annual.Nh50km.nc',
-        'MERRA': '/disks/arctic5_raid/abarrett/MERRA/daily/PRECTOT/MERRA.prod.PRECIP_STATS.assim.tavg1_2d_flx_Nx.accumulation.annual.Nh50km.nc4',
-        'MERRA2': '/disks/arctic5_raid/abarrett/MERRA2/daily/PRECTOT/MERRA2.tavg1_2d_flx_Nx.PRECIP_STATS.accumulation.annual.Nh50km.nc4',
-        'ERA5': '/projects/arctic_scientist_data/Reanalysis/ERA5/daily/TOTPREC/era5.single_level.PRECIP_STATS.accumulation.annual.Nh50km.nc4'}
+#fili = {'CFSR': '/disks/arctic5_raid/abarrett/CFSR/TOTPREC/CFSR.pgbh01.gdas.PRECIP_STATS.accumulation.annual.Nh50km.nc4',
+#        'ERAI': '/disks/arctic5_raid/abarrett/ERA_Interim/daily/PRECTOT/era_interim.PRECIP_STATS.accumulation.annual.Nh50km.nc',
+#        'JRA55': '/projects/arctic_scientist_data/Reanalysis/JRA55/daily/TOTPREC/JRA55.fcst_phy2m.PRECIP_STATS.accumulation.annual.Nh50km.nc',
+#        'MERRA': '/disks/arctic5_raid/abarrett/MERRA/daily/PRECTOT/MERRA.prod.PRECIP_STATS.assim.tavg1_2d_flx_Nx.accumulation.annual.Nh50km.nc4',
+#        'MERRA2': '/disks/arctic5_raid/abarrett/MERRA2/daily/PRECTOT/MERRA2.tavg1_2d_flx_Nx.PRECIP_STATS.accumulation.annual.Nh50km.nc4',
+#        'ERA5': '/projects/arctic_scientist_data/Reanalysis/ERA5/daily/TOTPREC/era5.single_level.PRECIP_STATS.accumulation.annual.Nh50km.nc4'}
 
 
 def precip_stats_to_time_series(reanalysis):
@@ -31,6 +32,8 @@ def precip_stats_to_time_series(reanalysis):
 
     if reanalysis == 'CFSR':
         if ('row' in ds.dims) & ('col' in ds.dims): ds.rename({'row': 'x', 'col': 'y'}, inplace=True)
+
+    ds['drizzle'] = ds.precTot - ds.wetdayTot
     
     dsMsk = ds * mask['ocean']
     dsSeries = dsMsk.mean(dim=['x','y'])
