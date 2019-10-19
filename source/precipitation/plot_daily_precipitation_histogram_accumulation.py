24e56eaf13fed01bfb26987a429008aa52e2bf0d
Andy Barrett <apbarret@nsidc.org>
2019-10-18 18:52:16 -0600

    added write function to get cdf stats

--- source/precipitation/plot_daily_precipitation_histogram_accumulation.py
+++ source/precipitation/plot_daily_precipitation_histogram_accumulation.py
@@ -18,6 +18,18 @@ filelist = {
 }
 
 
+def get_cdf_stats(reanalysis, pdf, cdf):
+    '''Generates a table of stats from cdf'''
+    pdriz = cdf.sel(bin_edge=1.1, method='nearest').values * 100.
+    pligt = cdf.sel(bin_edge=10.1, method='nearest').values * 100.
+    pplot = cdf.sel(bin_edge=6.1, method='nearest').values * 100.
+    vol = pdf * (pdf.bin_edge + 0.05)
+    vdriz = (vol.sel(bin_edge=slice(0., 1.)).sum() / vol.sum()).values * 100.
+    vligt = (vol.sel(bin_edge=slice(1.1, 10.)).sum() / vol.sum()).values * 100.
+    vhevy = (vol.sel(bin_edge=slice(10.1, None)).sum() / vol.sum()).values * 100.
+    print(f'{reanalysis:6} {pdriz:5.1f} {pligt:5.1f} {pplot:5.1f} {vdriz:5.1f} {vligt:5.1f} {vhevy:5.1f}')
+    
+
 def pdf2cdf(da):
     """Converts PDF of precip to normalized CDF"""
     y = da.sum(dim='time').cumsum()
@@ -29,7 +41,6 @@ def main(ybeg='1980', yend='2015'):
     fig, ax = plt.subplots(figsize=(10,10))
 
     for name, fp in filelist.items():
-
         da = xr.open_dataset(fp).sel(time=slice(ybeg, yend)).pdf
         da = da.loc[np.isin(da.time.dt.month, [8,9,10,11,12,1,2,3,4]),:]
         cdf = pdf2cdf(da)
@@ -37,11 +48,13 @@ def main(ybeg='1980', yend='2015'):
                 linewidth=2,
                 label=name.upper(),
                 color=reanalysis_color[name.upper()])
-
+        get_cdf_stats(name, da, cdf)
+        
     # Add observations
     da_obs = xr.open_dataset('~/data/SnowOnSeaIce/reanalysis_timeseries/npsnow_pdf.nc4')
     cdf_obs = da_obs.pdf.cumsum()
     cdf_obs = cdf_obs/cdf_obs[-1]
+    get_cdf_stats('Obs', da_obs.pdf, cdf_obs)
     ax.step(cdf_obs.bin_edge.values, cdf_obs.values,
             linewidth=2,
             label='NPSNOW',
@@ -51,7 +64,6 @@ def main(ybeg='1980', yend='2015'):
     ax.set_xlim(0.,6.)
     ax.set_ylabel('F', fontsize=20)
     ax.set_ylim(0.,1.)
-
     ax.tick_params(labelsize=15)
     
     ax.axvline(1., color='0.3', zorder=1)
@@ -60,7 +72,7 @@ def main(ybeg='1980', yend='2015'):
     #ax.set_title('Aug-Apr CDFs of Daily Precipitation for Arctic Ocean', fontsize=20)
     ax.legend(loc='lower right', fontsize=20)
 
-    fig.savefig('central_arctic_daily_precipitation_cdf_accumulation_period.png')
+    #fig.savefig('central_arctic_daily_precipitation_cdf_accumulation_period.png')
     
     plt.show()
 
